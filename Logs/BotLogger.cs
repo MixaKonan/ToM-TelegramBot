90efe6111c297414d93c607ecb109e105f354b02
Mykhailo Konontsev <fater181@gmail.com>
2020-11-13 23:28:24 +0200

    Simple file logging

---
+++ Logs/BotLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+
+namespace TomTelegramBot.Logs
+{
+    public class BotLogger
+    {
+        public static async void Log(string text)
+        {
+            const string path = "logs.txt";
+
+            if (File.Exists(path))
+            {
+                await File.AppendAllLinesAsync(path, new[] { text });
+            }
+            else
+            {
+                File.Create(path);
+                await File.AppendAllLinesAsync(path, new[] { text });
+            }
+        }
+    }
+}
